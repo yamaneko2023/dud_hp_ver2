eda3f62a96f78d8f7796588569455fd2a5d796c2
Nagatomi, Shuichi | BSD <ts-shuichi.nagatomi@rakuten.com>
2026-02-02 16:43:03 +0900

    removed 各ページへ素早くアクセス

--- app_hp/public/home.php
+++ app_hp/public/home.php
@@ -72,7 +72,6 @@ $ticker_json = json_encode($ticker_data);
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                             </svg>
                             <h3>サイトナビゲーション</h3>
-                            <p class="tile-header-desc">各ページへ素早くアクセス</p>
                         </div>
                         <div class="quick-links-grid">
                             <a href="vision.php" class="page-card">
